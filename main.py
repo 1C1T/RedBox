cc078702a818282b9247aa5b70054fb680f68661
1C1T <142925722+1C1T@users.noreply.github.com>
2023-08-26 22:47:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ import tkinter as tk
 import tkinter.font as tk_font
 from tkinter import (messagebox, filedialog, ttk)
 from tkinter.colorchooser import askcolor
-from yaml import load, dump, FullLoader
+from yaml import (load, dump, FullLoader)
 from pygments.lexers import LuaLexer
 from pygments import lex
 from ctypes import wintypes
 from PIL import Image
 from lua51 import LuaRuntime
+from pynput import (mouse, keyboard)
 import theRedBox
